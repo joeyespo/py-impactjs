9073c9c4be3e8e33d971764525954bdc1e4b3f38
Joe Esposito <joe@joeyespo.com>
2011-07-02 16:32:08 -0400

    Removed the static directory and changed the server code to handle the game list and the impactjs API redirects.

--- py_impactjs.py
+++ py_impactjs.py
@@ -4,86 +4,97 @@
 Impactjs Backend Server
 By Joe Esposito
 
-Implements a back-end server in Python for impactjs. This server also manages
-your games without any external dependencies or additional file manipulation.
+Implements a back-end server in Python for Impact by handling the original API
+originally implemented in PHP and allowing multiple games by re-routing URLs.
 """
 
-import os
-import codecs, json
-from flask import Flask, request, render_template, url_for, abort
+import os, glob, codecs, json
+from flask import Flask, abort, url_for, request, render_template, send_file, redirect
 
 # Constants
+SUPPORTED_EXTENSIONS = {'images': ['*.png', '*.gif', '*.jpg', '*.jpeg'], 'scripts': ['*.js']}
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-IMPACT_DIR = os.path.join(ROOT_DIR, 'impact')
-
-# Functions
-def static(filename):
-    """Provides the 'static' function that also appends the file's timestamp to the URL, usable in a template."""
-    url = url_for('.static', filename=filename)
-    fname = os.path.join(os.path.join(app.root_path, 'static'), filename)
-    st = int(os.path.getmtime(fname))
-    url += '?' + str(st)
-    return url
-
-def get_impact_file(filename):
-    """Gets the specified impact file, or the 404 page if it cannot be found."""
-    if '..' in filename or filename.startswith('/'):
-        abort(404)
-    path = os.path.join(IMPACT_DIR, filename)
-    try:
-        with codecs.open(path,'r','utf-8') as impact_file:
-            return impact_file.read()
-    except IOError:
-        return abort(404)
+GAMES_DIR = os.path.normpath(os.path.join(ROOT_DIR, 'games'))
+IMPACT_DIR = os.path.normpath(os.path.join(ROOT_DIR, 'impact')) + os.sep
+API_PATH = 'lib/weltmeister/api/'
 
 # Flask application
 app = Flask(__name__)
-app.jinja_env.globals.update(static=static)
 
 # Views
 @app.route('/')
-@app.route('/index.html')
 def index():
-    return render_template('index.html')
+    games = [game for game in os.listdir(GAMES_DIR) if os.path.isdir(os.path.join(GAMES_DIR, game))]
+    return render_template('index.html', games = games)
 
 @app.route('/new/')
-def play(game_name = None):
+def new_game(game_name = None):
     return '<em>TODO: Create a new game.</em>'
 
-@app.route('/play/')
-@app.route('/play/<game_name>')
-def play(game_name = None):
-    return '<em>TODO: Add a redirect to playing the selected game.</em>'
+@app.route('/games/')
+def games_index(game_path = None):
+    return redirect(url_for('index'))
 
-@app.route('/edit/')
-@app.route('/edit/<game_name>')
-def edit(game_name = None):
-    return '<em>TODO: Add a redirect to editing the selected game.</em>'
+@app.route('/games/<path:game_path>')
+def play_game(game_path = None):
+    game, action = os.path.split(os.path.normpath(game_path))
+    if os.path.split(game)[0] or (action and action != 'edit'):
+        abort(404)
+    return handle_impactjs('weltmeister.html' if action else 'index.html', game)
 
 @app.route('/impact/')
-@app.route('/impact/<path:impact_path>')
-def impact_file(impact_path = ''):
-    if not os.path.basename(impact_path):
-        impact_path = os.path.join(impact_path, 'index.html')
-    return get_impact_file(impact_path)
-
-# Override the Impact API
-@app.route('/impact/lib/weltmeister/api/glob.php')
-def impact_api(**args):
-    # TODO: Redirect 'game' to the current game (if any)
-    globs = request.args.get('glob[]', '')
-    if not globs:
-        return json.dump([])
-    globs = os.path.join(IMPACT_DIR, globs)
-    path, ext = os.path.split(globs)
-    files = os.listdir(path) if ext.startswith('*') else [globs]
-    return json.dumps(files)
-
-@app.route('/impact/lib/weltmeister/api/browse.php')
-def impact_api(**args):
-    # TODO: Redirect 'game' to the current game (if any)
-    return json.dumps([])
+@app.route('/impact/<path:impact_path>', methods=['GET', 'POST'])
+def handle_impactjs(impact_path = 'index.html', game = None):
+    # Method to reroute the game URLs
+    def reroute(path):
+        if game:
+            # TODO: implement
+            pass
+        return path
     
+    # Override the API
+    if impact_path.startswith(API_PATH):
+        method = impact_path[len(API_PATH):]
+        if method == 'glob.php':
+            # TODO: Handle arrays of 'glob' URL arguments
+            path = os.path.normpath(os.path.join(IMPACT_DIR, request.args.get('glob[]', ''))).replace('..', '')
+            # TODO: Really need to join with 'lib' here?
+            files = [f[len(os.path.join(IMPACT_DIR, 'lib') + os.sep):] for f in glob.glob(path)]
+            return json.dumps(files)
+        elif method == 'browse.php':
+            path = os.path.normpath(os.path.join(IMPACT_DIR, request.args.get('dir', ''))).replace('..', '')
+            exts = SUPPORTED_EXTENSIONS.get(request.args.get('type')) or ['*.*']
+            # Get the directories and files of the provided path, removing the base of the path (which leaves only the relative URL)
+            dirs = [dirname[len(IMPACT_DIR):] for dirname in os.listdir(path) if os.path.isdir(os.path.join(path, dirname))]
+            files = []
+            for ext in exts:
+                files += [filename[len(IMPACT_DIR):] for filename in glob.glob(os.path.join(path, ext))]
+            return json.dumps({'dirs': dirs, 'files': files, 'parent': path == IMPACT_DIR})
+        elif method == 'save.php' and request.method == 'POST':
+            # TODO: implement
+            path = request.args.get('path')
+            data = request.args.get('data')
+            if not path or not data:
+                return {'error': '1', 'msg': 'No Data or Path specified'}
+            elif not path.endswith('.js'):
+                return {'error': '3', 'msg': 'File must have a .js suffix'}
+            path = os.path.normpath(os.path.join(IMPACT_DIR, path)).replace('..', '')
+            # Write to file
+            try:
+                with open(path, 'w') as f:
+                    f.write(data)
+                return {'error': 0}
+            except:
+                return {'error': '2', 'msg': "Couldn't write to file: " + path}
+        else:
+            abort(404)
+    
+    # Get the local path and send back the specified file
+    path = os.path.normpath(os.path.join(IMPACT_DIR, impact_path))
+    print
+    print ' ', path
+    
+    return send_file(path)
 
 @app.errorhandler(404)
 def page_not_found(error):
